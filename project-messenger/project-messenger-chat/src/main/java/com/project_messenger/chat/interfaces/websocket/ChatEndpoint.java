521df473e234de0fa01278b6ef32370c8659a445
Vladislav Aleksandruk <1vladal1@gmail.com>
2018-10-21 21:20:33 +0300

    #Add WebSocket endpoint

---
+++ project-messenger/project-messenger-chat/src/main/java/com/project_messenger/chat/interfaces/websocket/ChatEndpoint.java
@@ -0,0 +1,31 @@
+package com.project_messenger.chat.interfaces.websocket;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/chat")
+public class ChatEndpoint {
+
+    @OnOpen
+    public void connect(final Session session) {
+
+    }
+
+    @OnMessage
+    public void receive(final Session session,
+                        final String message) {
+
+    }
+
+    @OnClose
+    public void close(final Session session,
+                      final CloseReason reason) {
+
+    }
+
+    @OnError
+    public void onError(final Session session,
+                        final Throwable error) {
+
+    }
+}
